5eff2cbab1e5503acb411c8474342d9341da2f84
Elliott Young <elliott.a.young@gmail.com>
2015-04-05 12:45:57 -0500

    Fixed Date Bug, works

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
   var currentTime = new Date();
   currentTime.getDate();
 
-  var dd = currentTime.getDate();
-  var mm = currentTime.getMonth() + 1;
+  var dd   = currentTime.getDate();
+  var mm   = currentTime.getMonth() + 1;
   var yyyy = currentTime.getFullYear();
 
-  var dateString = mm + "/" + dd + "/" + yyyy;
+  var dateString = Date.parse(currentTime);
+  var birthday   = Date.parse("4/13/2015");
 
-  if(dateString <= "4/13/2015"){
+  if(dateString >= birthday){
     $(".Answer").append("Yes");
     $(".Body").css("background-color", "#25A30F");
   }
@@ -17,3 +18,4 @@ $(document).ready(function(){
     $(".Body").css("background-color", "#D40D0D");
   }
 });
+
